a44546cbccd6f40438582ce3a16cbc1786384111
ahensTheGoodBoy <89793073+ahensTheGoodBoy@users.noreply.github.com>
2023-02-18 01:20:27 +0545

    Delete main_App directory

--- main_App/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import login_page, categorylist_page, add_category_page
-
-urlpatterns = [
-    path('', login_page, name="logIn"),
-    path('categories/', categorylist_page, name="categorylist"),
-    path('categories/add/', add_category_page, name="addcategory"),
-]
